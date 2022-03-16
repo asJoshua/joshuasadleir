4d741566b0cf7eda04dd20e3627c47ca892d2fec
asJoshua <joshuaandrewsadleir@gmail.com>
2022-03-16 12:50:36 +0100

    General Changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,27 @@
-/*=============== SHOW MENU ===============*/
+// Show Menu
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
 
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
+// Menu Show
 if(navToggle){
     navToggle.addEventListener('click', () =>{
         navMenu.classList.add('show-menu')
     })
 }
 
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
+// Menu Hidden
 if(navClose){
     navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
     })
 }
 
-/*=============== REMOVE MENU MOBILE ===============*/
+// Remove Menu on Mobile
 const navLink = document.querySelectorAll('.nav-link')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav-link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
